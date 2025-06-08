d41dc9cca46ce5806cacb5dd97bbdb13474fb623
Noah Lee <noah12lee@gmail.com>
2025-06-08 16:54:09 -0700

    binary operation support

--- clox/main.c
+++ clox/main.c
@@ -7,13 +7,32 @@ int main(int argc, const char* argv[]) {
 
   Chunk chunk;
   initChunk(&chunk);
+
+  /* Hand compiling AST:
+          -> 1.2 + 3.4
+  - -> /
+          -> 5.6
+  */
+
+
   int constant = addConstant(&chunk, 1.2);
   writeChunk(&chunk, OP_CONSTANT, 123);
   writeChunk(&chunk, constant, 123);
+  constant = addConstant(&chunk, 3.4);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_ADD, 123);
+
+  constant = addConstant(&chunk, 5.6);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_DIVIDE, 123);
+  writeChunk(&chunk, OP_NEGATE, 123);
 
   writeChunk(&chunk, OP_RETURN, 123);
 
-  disassembleChunk(&chunk, "test chunk");
   interpret(&chunk);
   freeVM();
   freeChunk(&chunk);
