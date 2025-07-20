c3d84ca83ac3fbd04e786c564499823641eda593
Noah Lee <noah12lee@gmail.com>
2025-07-20 15:17:50 -0700

    closures

--- clox/memory.c
+++ clox/memory.c
@@ -30,8 +30,19 @@ static void freeObject(Obj* object) {
       FREE(ObjFunction, object);
       break;
     }
-    case OBJ_NATIVE:
+    case OBJ_NATIVE: {
       FREE(ObjNative, object);
+      break;
+    }
+    case OBJ_CLOSURE: {
+      ObjClosure* closure = (ObjClosure*)object;
+      FREE_ARRAY(ObjUpvalue*, closure->upvalues,
+                 closure->upvalueCount);
+      FREE(ObjClosure, object);
+      break;
+    }
+    case OBJ_UPVALUE:
+      FREE(ObjUpvalue, object);
       break;
   }
 }
